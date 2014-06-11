cbb60e4f26ffee49f73bfe4c14cbd45a67798050
Albatross <albatross@nosuch.net>
2014-06-10 22:23:23 -0700

    Started work on the new portal crystal.

---
+++ java/iceandshadow2/nyx/items/NyxInPortalCrystal.java
@@ -0,0 +1,12 @@
+package iceandshadow2.nyx.items;
+
+import iceandshadow2.ias.bases.IaSItemSingle;
+import iceandshadow2.util.EnumIaSModule;
+
+public class NyxInPortalCrystal extends IaSItemSingle {
+
+	public NyxInPortalCrystal(String texName) {
+		super(EnumIaSModule.MODULE_NYX, texName);
+	}
+
+}
