73e889c2faad257dbcf6cc42fea5e23299b4a9b5
TheRabbitologist <albatross@nosuch.net>
2014-09-09 01:30:34 -0700

    Fixed missing import.

--- java/iceandshadow2/IceAndShadow2.java
+++ java/iceandshadow2/IceAndShadow2.java
@@ -7,6 +7,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.relauncher.Side;
