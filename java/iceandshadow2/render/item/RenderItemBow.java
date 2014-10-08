fda2dc011d125cc1a3168456e1479a9802e259eb
TheRabbitologist <albatross@nosuch.net>
2014-10-08 13:27:48 -0700

    RenderItemBow now extends from RenderItemVanillaGlowing.
    Removed RenderItemVanilla. It was old cruft by now.

--- java/iceandshadow2/render/item/RenderItemBow.java
+++ java/iceandshadow2/render/item/RenderItemBow.java
@@ -22,53 +22,48 @@ import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.client.IItemRenderer.ItemRenderType;
 
-public class RenderItemBow extends RenderItemVanilla {
+public class RenderItemBow extends RenderItemVanillaGlowing {
 	private boolean lb;
 
 	public RenderItemBow(boolean isLongbow) {
-		this.mc = Minecraft.getMinecraft();
 		lb = isLongbow;
 	}
 
 	@Override
-	public boolean handleRenderType(ItemStack item, ItemRenderType type) {
-		return type == ItemRenderType.EQUIPPED;
-	}
-
-	@Override
-	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
-			ItemRendererHelper helper) {
-		return false;
-	}
-
-	@Override
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
-		GL11.glPopMatrix(); //STAHP PRE-TRANSLATING MY BOW!
-		GL11.glPushMatrix();
-		 
-		float f2 = 3F - (1F/3F);
-		GL11.glRotatef(-20.0F, 0.0F, 0.0F, 1.0F);
-		GL11.glRotatef(90.0F, 1.0F, 0.0F, 0.0F);
-		GL11.glRotatef(-60.0F, 0.0F, 0.0F, 1.0F);
-		GL11.glScalef(f2, f2, f2);
-		GL11.glTranslatef(-0.25F, -0.1875F, 0.1875F);
-		
-		//Standard bow transforms.
-		if(!lb) {
-			GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
-			GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
-			GL11.glScalef(0.625F, -0.625F, 0.625F);
+
+		if(type == ItemRenderType.EQUIPPED) {
+			GL11.glPopMatrix(); //STAHP PRE-TRANSLATING MY BOW!
+			GL11.glPushMatrix();
+			
+			float f2 = 3F - (1F/3F);
+			GL11.glRotatef(-20.0F, 0.0F, 0.0F, 1.0F);
+			GL11.glRotatef(90.0F, 1.0F, 0.0F, 0.0F);
+			GL11.glRotatef(-60.0F, 0.0F, 0.0F, 1.0F);
+			GL11.glScalef(f2, f2, f2);
+			GL11.glTranslatef(-0.25F, -0.1875F, 0.1875F);
+
+			//Standard bow transforms.
+			if(!lb) {
+				GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
+				GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
+				GL11.glScalef(0.625F, -0.625F, 0.625F);
+			}
+			else {
+				GL11.glTranslatef(0.0F, 0.125F, 0.5F);
+				GL11.glScalef(0.625F, -0.5F, 0.875F);
+			}
+			GL11.glRotatef(-110.0F, 1.0F, 0.0F, 0.0F);
+			GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+			GL11.glPushMatrix();
 		}
-		else {
-			GL11.glTranslatef(0.0F, 0.125F, 0.5F);
-			GL11.glScalef(0.625F, -0.5F, 0.875F);
-		}
-		GL11.glRotatef(-110.0F, 1.0F, 0.0F, 0.0F);
-		GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
 		
 		super.renderItem(type, item, data);
 		
-		GL11.glPopMatrix();
-		GL11.glPushMatrix();
+		if(type == ItemRenderType.EQUIPPED) {
+			GL11.glPopMatrix();
+			GL11.glPopMatrix();
+			GL11.glPushMatrix();
+		}
 	}
 }
