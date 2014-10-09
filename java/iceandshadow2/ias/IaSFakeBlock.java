d4fafb3b64b88c71d3b0378acaa7677c67720f4c
TheRabbitologist <albatross@nosuch.net>
2014-10-08 22:51:56 -0700

    Fixed a bug that was preventing multiplayer initialization.
    Is it just me, or is this the longest IaS worlds have ever taken to
    initialize?

--- java/iceandshadow2/ias/IaSFakeBlock.java
+++ java/iceandshadow2/ias/IaSFakeBlock.java
@@ -1,11 +1,14 @@
 package iceandshadow2.ias;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import iceandshadow2.IceAndShadow2;
 import iceandshadow2.util.EnumIaSModule;
 import iceandshadow2.util.IaSRegistration;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 
 public class IaSFakeBlock extends Block {
 	
@@ -17,6 +20,6 @@ public class IaSFakeBlock extends Block {
 	}
 	
 	public Item getItem() {
-		return this.getItem(null, 0, 0, 0);
+		return Item.getItemFromBlock(this);
 	}
 }
