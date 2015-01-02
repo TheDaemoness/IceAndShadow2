a6af60d831ab5076bf793612a8bbd454a21258d7
TheRabbitologist <albatross@nosuch.net>
2015-01-02 12:32:38 -0800

    Refactored the material base class.

--- java/iceandshadow2/api/IaSToolMaterial.java
+++ java/iceandshadow2/api/IaSToolMaterial.java
@@ -20,19 +20,6 @@ import cpw.mods.fml.relauncher.SideOnly;
  */
 public abstract class IaSToolMaterial implements IIaSXpAltarSacrifice {
 
-	@SideOnly(Side.CLIENT)
-	protected IIcon iconTool[], iconWeapon[], iconTBroken[], iconWBroken[];
-
-	/**
-	 * Called when a tool breaks to determine whether or not a "broken" tool item should be given.
-	 * @param is The tool being used to harvest.
-	 * @param user The user of the tool.
-	 * @return True if a broken tool item should be returned, false otherwise.
-	 */
-	public boolean getBrokenTool(ItemStack is, EntityLivingBase user) {
-		return true;
-	}
-
 	public static IaSToolMaterial extractMaterial(ItemStack is) {
 		if(is == null)
 			return IaSRegistry.getDefaultMaterial();
@@ -46,22 +33,74 @@ public abstract class IaSToolMaterial implements IIaSXpAltarSacrifice {
 		return m;
 	}
 
+	@SideOnly(Side.CLIENT)
+	protected IIcon iconTool[], iconWeapon[], iconTBroken[], iconWBroken[];
+
 	/**
-	 * Gets the prefix associated with the textures for the tool.
-	 * This would be something like "IceAndShadow2:iasTool".
-	 * The tool class and tool material name would then be tacked on to that name.
-	 * The final string texture ends up looking something like "IceAndShadow2:iasToolEchirPickaxe".
+	 * Called by the default implementation of onAttack to determine how much damage a tool takes from attacking.
+	 * @param is
+	 * @param user
+	 * @param target
+	 * @return
 	 */
-	public String getTextureNamePrefix() {
-		return "IceAndShadow2:tools/iasTool";
+	public int damageToolOnAttack(ItemStack is, EntityLivingBase user, Entity target) {
+		return 1;
 	}
 
 	/**
-	 * Gets the material name associated with a tool, for the purposes of unlocalized names and texture names.
-	 * This should be capitalized, but not allcaps.
-	 * The tool name will end up something like item.iasToolMaterialClass.name
+	 * Gets the item's base attack damage.
+	 * This is called by the default implementations of getToolDamage() and getKnifeDamage().
+	 * Players are not forced to override it if it won't be used. 
+	 * @return 
 	 */
-	public abstract String getMaterialName();
+	public float getBaseDamage() {
+		return 3.0F;
+	}
+
+	/**
+	 * Gets the tool's base harvest level. Used by the default implementation of getHarvestLevel().
+	 * Cheat sheet: -1 = fists, 0 = wood, 1 = stone, 2 = iron, 3 = diamond
+	 */
+	public int getBaseLevel() {
+		return 0;
+	}
+
+	/**
+	 * Gets the item's base mining speed. Used in default implementations of getHarvestSpeed.
+	 * @return The number of times faster than hand this tool is.
+	 */
+	public abstract float getBaseSpeed();
+
+	/**
+	 * Called when a tool breaks to determine whether or not a "broken" tool item should be given.
+	 * @param is The tool being used to harvest.
+	 * @param user The user of the tool.
+	 * @return True if a broken tool item should be returned, false otherwise.
+	 */
+	public boolean getBrokenTool(ItemStack is, EntityLivingBase user) {
+		return true;
+	}
+
+	/**
+	 * Get the tool's maximum durability.
+	 * @param is The tool.
+	 * @return The tool's durability. Each point of durability is one use.
+	 */
+	public abstract int getDurability(ItemStack is);
+
+	/**
+	 * Gets the tool's harvest level. This determines what materials it can mine.
+	 * Cheat sheet: -1 = fists, 0 = wood, 1 = stone, 2 = iron, 3 = diamond
+	 * Default implementation mimics vanilla behavior.
+	 * @param is The stack.
+	 * @param toolClass The user.
+	 * @return The tool's mining level.
+	 */
+	public int getHarvestLevel(ItemStack is, String toolClass) {
+		if(is.getItem().getToolClasses(is).contains(toolClass))
+			return this.getBaseLevel();
+		return -1;
+	}
 
 	/**
 	 * Gets the item's mine speed against a target block.
@@ -77,19 +116,73 @@ public abstract class IaSToolMaterial implements IIaSXpAltarSacrifice {
 	}
 
 	/**
-	 * Gets the item's base mining speed. Used in default implementations of getHarvestSpeed.
-	 * @return The number of times faster than hand this tool is.
+	 * Get an icon for an item stack.
 	 */
-	public abstract float getBaseSpeed();
+	public IIcon getIcon(ItemStack is) {
+		EnumIaSToolClass t = ((IIaSTool)is.getItem()).getIaSToolClass();
+		if(t.isWeapon())
+			return iconWeapon[t.getClassId()];
+		else
+			return iconTool[t.getClassId()];
+	}
 
 	/**
-	 * Gets the item's base attack damage.
-	 * This is called by the default implementations of getToolDamage() and getKnifeDamage().
-	 * Players are not forced to override it if it won't be used. 
-	 * @return 
+	 * Called to determine the cooldown time of the throwing knife.
+	 * @param par1ItemStack
+	 * @param par2World
+	 * @param par3EntityPlayer
+	 * @return
 	 */
-	public float getBaseDamage() {
-		return 3.0F;
+	public int getKnifeCooldown(ItemStack par1ItemStack, World par2World,
+			EntityPlayer par3EntityPlayer) {
+		return 12;
+	}
+
+	/**
+	 * Gets a thrown throwing knife's damage bonus against certain entities.
+	 * This is NOT called by left-click attacks.
+	 * @param knife The in flight knife.
+	 * @param user The user.
+	 * @param target The target.
+	 * @return The damage that the knife should do. Note that this is pure damage, not bonus damage, so returning 0.0F will cause the tool to do no damage.
+	 */
+	public float getKnifeDamage(IaSEntityKnifeBase knife, EntityLivingBase user, Entity target) {
+		return getBaseDamage()+2;
+	}
+
+	/**
+	 * Called when a throwing knife collides with an entity or block and onThrowingKnifeHit returns true.
+	 * Used to determine the item stack that the knife should drop.
+	 * @param user The thrower.
+	 * @param knife The throwing knife entity.
+	 * @return The item stack to drop when the knife hits something, or null if no drop.
+	 */
+	public ItemStack getKnifeDrop(EntityLivingBase user, IaSEntityKnifeBase knife, MovingObjectPosition target) {
+		return knife.getItemStack();
+	}
+
+	/**
+	 * Gets the material name associated with a tool, for the purposes of unlocalized names and texture names.
+	 * This should be capitalized, but not allcaps.
+	 * The tool name will end up something like item.iasToolMaterialClass.name
+	 */
+	public abstract String getMaterialName();
+
+	/**
+	 * Called to get tool material rarity.
+	 */
+	public EnumRarity getRarity() {
+		return EnumRarity.common;
+	}
+
+	/**
+	 * Gets the prefix associated with the textures for the tool.
+	 * This would be something like "IceAndShadow2:iasTool".
+	 * The tool class and tool material name would then be tacked on to that name.
+	 * The final string texture ends up looking something like "IceAndShadow2:iasToolEchirPickaxe".
+	 */
+	public String getTextureNamePrefix() {
+		return "IceAndShadow2:tools/iasTool";
 	}
 
 	/**
@@ -116,56 +209,42 @@ public abstract class IaSToolMaterial implements IIaSXpAltarSacrifice {
 		return getBaseDamage();
 	}
 
-	/**
-	 * Gets a thrown throwing knife's damage bonus against certain entities.
-	 * This is NOT called by left-click attacks.
-	 * @param knife The in flight knife.
-	 * @param user The user.
-	 * @param target The target.
-	 * @return The damage that the knife should do. Note that this is pure damage, not bonus damage, so returning 0.0F will cause the tool to do no damage.
-	 */
-	public float getKnifeDamage(IIaSThrowingKnife knife, EntityLivingBase user, Entity target) {
-		return getBaseDamage()+2;
+	public String getUnlocalizedName(ItemStack is) {
+		EnumIaSToolClass t = ((IIaSTool)is.getItem()).getIaSToolClass();
+		return "item.iasTool"+this.getMaterialName()+t.toString();
 	}
 
 	/**
-	 * Gets the tool's harvest level. This determines what materials it can mine.
-	 * Cheat sheet: -1 = fists, 0 = wood, 1 = stone, 2 = iron, 3 = diamond
-	 * Default implementation mimics vanilla behavior.
-	 * @param is The stack.
-	 * @param toolClass The user.
-	 * @return The tool's mining level.
+	 * Indicates whether or not this tool glows.
 	 */
-	public int getHarvestLevel(ItemStack is, String toolClass) {
-		if(is.getItem().getToolClasses(is).contains(toolClass))
-			return this.getBaseLevel();
-		return -1;
-	}
-
-	/**
-	 * Gets the tool's base harvest level. Used by the default implementation of getHarvestLevel().
-	 * Cheat sheet: -1 = fists, 0 = wood, 1 = stone, 2 = iron, 3 = diamond
-	 */
-	public int getBaseLevel() {
-		return 0;
-	}
-
-	/**
-	 * Get the tool's maximum durability.
-	 * @param is The tool.
-	 * @return The tool's durability. Each point of durability is one use.
-	 */
-	public abstract int getDurability(ItemStack is);
-
-	/**
-	 * Called when a tool is swung.
-	 * @param is The tool being swing
-	 * @param user The user of the tool.
-	 * @return True if further processing should be canceled.
-	 */
-	public boolean onSwing(ItemStack is, EntityLivingBase user) {
+	public boolean glows(EnumIaSToolClass mat) {
 		return false;
 	}
+
+	/**
+	 * Gets whether or not a tool can be repaired in an anvil.
+	 */
+	public boolean isRepairable(ItemStack tool, ItemStack mat) {
+		return false;
+	}
+
+	/**
+	 * Called whenever the tool does is swung at an entity. This is NOT called when a thrown knife hits its target.
+	 * @param is The item stack the player used to hit.
+	 * @param user The player using the tool.
+	 * @param target The entity damaged by the tool.
+	 * @return The number of points of durability that should be deducted by this left-click. This is ignored by throwing knives.
+	 */
+	public int onAttack(ItemStack is, EntityLivingBase user, Entity target) {
+		if(target instanceof EntityLivingBase) {
+			if(user instanceof EntityPlayer)
+				target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer)user), getToolDamage(is, user, target));
+			else
+				target.attackEntityFrom(DamageSource.causeMobDamage(user), getToolDamage(is, user, target));
+		}
+		return damageToolOnAttack(is,user,target);
+	}
+
 
 	/**
 	 * Called when a tool successfully harvests a block.
@@ -179,10 +258,23 @@ public abstract class IaSToolMaterial implements IIaSXpAltarSacrifice {
 	}
 
 	/**
-	 * Gets whether or not a tool can be repaired in an anvil.
+	 * Called when a throwing knife collides with an entity or block.
+	 * @param user The thrower.
+	 * @param knife The throwing knife entity.
+	 * @param target The block or entity hit.
 	 */
-	public boolean isRepairable(ItemStack tool, ItemStack mat) {
-		return false;
+	public void onKnifeHit(EntityLivingBase user, IaSEntityKnifeBase knife, MovingObjectPosition target) {
+		return;
+	}
+
+	/**
+	 * Called when a throwing knife is thrown, just before the entity is spawned into the world.
+	 * @param is The item stack the player was going to throw.
+	 * @param user The entity using the throwing knives.
+	 * @param knife The knife entity that will be spawned.
+	 */
+	public void onKnifeThrow(ItemStack is, EntityLivingBase user, IaSEntityKnifeBase knife) {
+		return;
 	}
 
 	/**
@@ -207,105 +299,12 @@ public abstract class IaSToolMaterial implements IIaSXpAltarSacrifice {
 	}
 
 	/**
-	 * Called when an intelligent Nyxian enemy is going to pick up this tool (namely, Winter Skeletons).
-	 * Note that onRightClick() will NOT get called while the tool is being used by a intelligent Nyxian enemy.
-	 * All other combat-related callbacks work fine, however.
-	 * @param is The item stack the entity is going to pick up.
-	 * @param user The entity that is going to pick up this tool.
-	 * @return True if the Nyxian enemy should pick up and use this tool, false otherwise.
+	 * Called when a tool is swung.
+	 * @param is The tool being swing
+	 * @param user The user of the tool.
+	 * @return True if further processing should be canceled.
 	 */
-	public boolean shouldEnemiesUse(ItemStack is, EntityLivingBase user) {
-		return false;
-	}
-
-	/**
-	 * Called whenever the tool does is swung at an entity. This is NOT called when a thrown knife hits its target.
-	 * @param is The item stack the player used to hit.
-	 * @param user The player using the tool.
-	 * @param target The entity damaged by the tool.
-	 * @return The number of points of durability that should be deducted by this left-click. This is ignored by throwing knives.
-	 */
-	public int onAttack(ItemStack is, EntityLivingBase user, Entity target) {
-		if(target instanceof EntityLivingBase) {
-			if(user instanceof EntityPlayer)
-				target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer)user), getToolDamage(is, user, target));
-			else
-				target.attackEntityFrom(DamageSource.causeMobDamage(user), getToolDamage(is, user, target));
-		}
-		return damageToolOnAttack(is,user,target);
-	}
-
-	/**
-	 * Called by the default implementation of onAttack to determine how much damage a tool takes from attacking.
-	 * @param is
-	 * @param user
-	 * @param target
-	 * @return
-	 */
-	public int damageToolOnAttack(ItemStack is, EntityLivingBase user, Entity target) {
-		return 1;
-	}
-
-	/**
-	 * Called when a throwing knife is thrown, just before the entity is spawned into the world.
-	 * @param is The item stack the player was going to throw.
-	 * @param user The entity using the throwing knives.
-	 */
-	public void onThrowingKnifeThrow(ItemStack is, EntityLivingBase user, IIaSThrowingKnife knife) {
-		return;
-	}
-
-
-	/**
-	 * Called when a throwing knife collides with an entity or block.
-	 * Note that returning false with this may result in this function being called a number of times if knife passes through its target.
-	 * @param user The thrower.
-	 * @param knife The throwing knife entity.
-	 * @return True if the throwing knife should drop as an item stack (or despawn), false if the knife should continue living.
-	 */
-	public boolean onThrowingKnifeHit(EntityLivingBase user, IIaSThrowingKnife knife, MovingObjectPosition target) {
-		return true;
-	}
-
-	/**
-	 * Called when a throwing knife collides with an entity or block and onThrowingKnifeHit returns true.
-	 * Used to determine the item stack that the knife should drop.
-	 * @param user The thrower.
-	 * @param knife The throwing knife entity.
-	 * @return The item stack to drop when the knife hits something (if onThrowingKnifeHit is true), or null if no drop.
-	 */
-	public ItemStack getThrowingKnifeDrop(EntityLivingBase user, IIaSThrowingKnife knife, MovingObjectPosition target) {
-		return getDefaultThrowingKnifeDrop(knife);
-	}
-
-	/**
-	 * Helper function to get the default item stack drop of a throwing knife.
-	 * @param knife The throwing knife entity.
-	 */
-	protected static final ItemStack getDefaultThrowingKnifeDrop(IIaSThrowingKnife knife) {
-		return null;
-	}
-
-	public String getUnlocalizedName(ItemStack is) {
-		EnumIaSToolClass t = ((IIaSTool)is.getItem()).getIaSToolClass();
-		return "item.iasTool"+this.getMaterialName()+t.toString();
-	}
-
-	/**
-	 * Get an icon for an item stack.
-	 */
-	public IIcon getIcon(ItemStack is) {
-		EnumIaSToolClass t = ((IIaSTool)is.getItem()).getIaSToolClass();
-		if(t.isWeapon())
-			return iconWeapon[t.getClassId()];
-		else
-			return iconTool[t.getClassId()];
-	}
-
-	/**
-	 * Indicates whether or not this tool glows.
-	 */
-	public boolean glows(EnumIaSToolClass mat) {
+	public boolean onSwing(ItemStack is, EntityLivingBase user) {
 		return false;
 	}
 
@@ -330,9 +329,14 @@ public abstract class IaSToolMaterial implements IIaSXpAltarSacrifice {
 	}
 
 	/**
-	 * Called to get tool material rarity.
+	 * Called when an intelligent Nyxian enemy is going to pick up this tool (namely, Winter Skeletons).
+	 * Note that onRightClick() will NOT get called while the tool is being used by a intelligent Nyxian enemy.
+	 * All other combat-related callbacks work fine, however.
+	 * @param is The item stack the entity is going to pick up.
+	 * @param user The entity that is going to pick up this tool.
+	 * @return True if the Nyxian enemy should pick up and use this tool, false otherwise.
 	 */
-	public EnumRarity getRarity() {
-		return EnumRarity.common;
+	public boolean shouldEnemiesUse(ItemStack is, EntityLivingBase user) {
+		return false;
 	}
 }
