a652860a5f24e4ca04bc41928a695f377c594005
TheRabbitologist <albatross@nosuch.net>
2015-01-08 09:57:09 -0800

    Ice And Shadow now has some nice formatting for its messages.

--- java/iceandshadow2/util/IaSPlayerHelper.java
+++ java/iceandshadow2/util/IaSPlayerHelper.java
@@ -3,14 +3,19 @@ package iceandshadow2.util;
 import iceandshadow2.IaSFlags;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentStyle;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 
 public class IaSPlayerHelper {
 	private static boolean dochat = true;
 	
 	public static void messagePlayer(EntityPlayer plai, String str) {
-		if(dochat && plai.worldObj.isRemote && IaSFlags.flag_help_messages) {
-			plai.addChatMessage(new ChatComponentText(str));
+		if(dochat && plai.worldObj.isRemote) {
+			ChatComponentText txt = new ChatComponentText(str);
+			txt.setChatStyle(new ChatStyle().setItalic(true).setColor(EnumChatFormatting.GRAY));
+			plai.addChatMessage(txt);
 			dochat = false;
 		}
 		else
