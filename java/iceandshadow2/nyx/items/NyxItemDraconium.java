dbc7709c05973782a6719656fd64d37ed983828a
TheRabbitologist <albatross@nosuch.net>
2015-01-12 14:40:09 -0800

    Draconium now drops from ore, glows, and has textures.

--- java/iceandshadow2/nyx/items/NyxItemDraconium.java
+++ java/iceandshadow2/nyx/items/NyxItemDraconium.java
@@ -1,13 +1,39 @@
 package iceandshadow2.nyx.items;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.item.ItemStack;
 import iceandshadow2.EnumIaSModule;
+import iceandshadow2.ias.interfaces.IIaSGlowing;
 import iceandshadow2.ias.items.IaSBaseItemSingle;
 
-public class NyxItemDraconium extends IaSBaseItemSingle {
+public class NyxItemDraconium extends IaSBaseItemSingle implements IIaSGlowing {
 
 	public NyxItemDraconium(String texName) {
 		super(EnumIaSModule.NYX, texName);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean requiresMultipleRenderPasses() {
+		return true;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public int getRenderPasses(int metadata) {
+		return 2;
+	}
+
+	@Override
+	public int getFirstGlowPass(ItemStack is) {
+		return 1;
+	}
+
+	@Override
+	public boolean usesDefaultGlowRenderer() {
+		return true;
 	}
 
 }
