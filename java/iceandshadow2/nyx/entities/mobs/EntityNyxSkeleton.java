6c976a1aaa7685e0641dd0467e464b2581f0d708
TheRabbitologist <albatross@nosuch.net>
2015-08-03 18:27:19 -0700

    Nyxian skeletons now call for help when attacked.

--- java/iceandshadow2/nyx/entities/mobs/EntityNyxSkeleton.java
+++ java/iceandshadow2/nyx/entities/mobs/EntityNyxSkeleton.java
@@ -123,7 +123,7 @@ IIaSMobGetters {
 		this.tasks.addTask(6, new EntityAIWatchClosest(this,
 				EntityPlayer.class, 8.0F));
 		this.tasks.addTask(6, new EntityAILookIdle(this));
-		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
 		this.targetTasks.addTask(2, new EntityAINyxTargeter(this));
 
 		if (par1World != null && !par1World.isRemote)
