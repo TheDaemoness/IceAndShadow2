8e6f9bb5394a4fbd67b7d7af2062bf9a75f21462
TheRabbitologist <albatross@nosuch.net>
2015-09-19 20:48:29 -0700

    Tweaked thermal core size, lighting, and effects.

--- java/iceandshadow2/nyx/blocks/NyxBlockThermalCore.java
+++ java/iceandshadow2/nyx/blocks/NyxBlockThermalCore.java
@@ -31,43 +31,45 @@ import net.minecraftforge.common.util.ForgeDirection;
 public class NyxBlockThermalCore extends IaSBaseBlockMulti {
 	public NyxBlockThermalCore(String id) {
 		super(EnumIaSModule.NYX, id, Material.fire, 2);
-		this.setLuminescence(0.5F);
 		this.setLightOpacity(0);
 		this.setHardness(0.5F);
 		this.setLightColor(1.0F, 0.5F, 0.0F);
 		this.setResistance(NyxBlockStone.RESISTANCE);
-		this.setBlockBounds(0.2F, 0.0F, 0.2F, 0.8F, 0.6F, 0.8F);
+		this.setBlockBounds(0.3F, 0.0F, 0.3F, 0.7F, 0.4F, 0.7F);
 		this.setTickRandomly(true);
 	}
-	
-	
+
+	@Override
+	public int getLightValue(IBlockAccess world, int x, int y, int z) {
+		if(world.getBlockMetadata(x, y, z) == 1)
+			return 7;
+		return 3;
+	}
+
+	@Override
+	protected boolean canSilkHarvest() {
+		return false;
+	}
 
 	@Override
 	public boolean onBlockActivated(World w, int x,
 			int y, int z, EntityPlayer ep,
 			int p_149727_6_, float p_149727_7_, float p_149727_8_,
 			float p_149727_9_) {
-		w.setBlockToAir(x, y, z);
-		IaSPlayerHelper.giveItem(ep, new ItemStack(NyxItems.alabaster,1,1));
+		if(w.getBlockMetadata(x, y, z) == 1)
+			return false;
+		w.func_147480_a(x, y, z, false);
+		w.setBlock(x, y, z, this, 1, 0x2);
+		IaSFxManager.spawnParticle(w, "shadowSmokeLarge",
+				x+0.5, y+0.5, z+0.5, false, true);
+		updateTick(w, x, y, z, w.rand);
 		return true;
 	}
-
-
 
 	@Override
 	public void onBlockDestroyedByExplosion(World w, int x, int y, int z,
 			Explosion e) {
 		w.setBlockToAir(x, y, z);
-	}
-
-	@Override
-	public void onBlockDestroyedByPlayer(World w, int x, int y, int z, int meta) {
-		if (meta == 0) {
-			w.setBlock(x, y, z, this, 1, 0x2);
-			IaSFxManager.spawnParticle(w, "shadowSmokeLarge",
-					x+0.5, y+0.5, z+0.5, false, true);
-			updateTick(w, x, y, z, w.rand);
-		}
 	}
 	
 	@Override
@@ -76,9 +78,11 @@ public class NyxBlockThermalCore extends IaSBaseBlockMulti {
 		if(w.getBlockMetadata(x, y, z) != 1)
 			return;
 		for(int xit = -1; xit<=1; ++xit) {
-			for(int zit = -1; zit<=1; ++zit) {
-				if(NyxBlockThermalAir.canReplace(w, x+xit, y+1, z+zit))
-					w.setBlock(x+xit, y+1, z+zit, NyxBlocks.thermalAir, 7, 0x3);
+			for(int yit = -1; yit<=1; ++yit) {
+				for(int zit = -1; zit<=1; ++zit) {
+					if(NyxBlockThermalAir.canReplace(w, x+xit, y+yit, z+zit))
+						w.setBlock(x+xit, y+1, z+zit, NyxBlocks.thermalAir, 7, 0x3);
+				}
 			}
 		}
 	}
@@ -96,6 +100,8 @@ public class NyxBlockThermalCore extends IaSBaseBlockMulti {
 	@Override
 	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z,
 			int metadata, int fortune) {
+		if(world.getBlockMetadata(x,y,z) == 0)
+			return super.getDrops(world, x, y, z, metadata, fortune);
 		return new ArrayList<ItemStack>();
 	}
 
