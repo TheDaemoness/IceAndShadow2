bf6ca3f11e36cde290705ce1c9bfa995754a18fa
TheRabbitologist <admin@rabbitology.net>
2015-11-24 14:09:27 -0800

    Fixed a multiplayer bug with IaSSenseActions.

--- java/iceandshadow2/nyx/entities/ai/senses/IaSSenseActions.java
+++ java/iceandshadow2/nyx/entities/ai/senses/IaSSenseActions.java
@@ -2,6 +2,7 @@ package iceandshadow2.nyx.entities.ai.senses;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class IaSSenseActions extends IaSSense {
 
@@ -11,9 +12,13 @@ public class IaSSenseActions extends IaSSense {
 
 	@Override
 	public boolean canSense(Entity ent) {
-		if(ent instanceof EntityLivingBase) {
-			if(((EntityLivingBase)ent).isSwingInProgress || ent.isEating())
-				return isInRange(ent);
+		if(isInRange(ent)) {
+			if(ent instanceof EntityLivingBase) {
+				if(((EntityLivingBase)ent).isSwingInProgress)
+					return true;
+				if(ent instanceof EntityPlayer)
+					return ((EntityPlayer)ent).isUsingItem();
+			}
 		}
 		return false;
 	}
