4051bdeef0f136658001f7692daf0ec799f21863
TheDaemoness <admin@rabbitology.net>
2017-11-30 18:22:01 -0800

    Buffed gossamer armor.
    Upon taking damage while wielding gossamer armor, you gain brief invisibility. The more pieces you wear, the longer the invisibility.
    A full set of gossamer armor gives 4 seconds of invisibility on damage and reduces magic damage by up to 4 hearts.

--- java/iceandshadow2/nyx/forge/NyxArmorHandler.java
+++ java/iceandshadow2/nyx/forge/NyxArmorHandler.java
@@ -2,45 +2,46 @@ package iceandshadow2.nyx.forge;
 
 import iceandshadow2.ias.items.tools.IaSTools;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class NyxArmorHandler {
-
+	
 	@SubscribeEvent
 	public void dmg(LivingHurtEvent e) {
 		if(e.entity.worldObj.isRemote)
 			return;
 		final EntityLivingBase elb = e.entityLiving;
-		if(elb != null && e.source.isMagicDamage()) {
-			if(e.source.isDamageAbsolute())
-				return;
-			int pieces = 0;
-			if(elb.getEquipmentInSlot(1) != null &&
-					elb.getEquipmentInSlot(1).getItem() == IaSTools.armorSpiderSilk[3]) {
-				e.ammount -= 1;
-				++pieces;
-				elb.getEquipmentInSlot(1).damageItem(1, elb);
-			}
-			if(elb.getEquipmentInSlot(2) != null &&
-					elb.getEquipmentInSlot(2).getItem() == IaSTools.armorSpiderSilk[2]) {
-				e.ammount -= 3;
-				++pieces;
-				elb.getEquipmentInSlot(2).damageItem(3, elb);
-			}
-			if(elb.getEquipmentInSlot(3) != null &&
-					elb.getEquipmentInSlot(3).getItem() == IaSTools.armorSpiderSilk[1]) {
-				e.ammount -= 2;
-				++pieces;
-				elb.getEquipmentInSlot(3).damageItem(2, elb);
-			}
-			if(elb.getEquipmentInSlot(4) != null &&
-					elb.getEquipmentInSlot(4).getItem() == IaSTools.armorSpiderSilk[0]) {
-				e.ammount -= 1;
-				++pieces;
-				//Boots deliberately do not get damaged.
-			}
-			e.ammount = Math.max(1, e.ammount-pieces/2);
+		if(elb == null)
+			return;
+		if(e.source.isDamageAbsolute())
+			return;
+		int protection=0;
+		if(elb.getEquipmentInSlot(1) != null &&
+			elb.getEquipmentInSlot(1).getItem() == IaSTools.armorSpiderSilk[3]) {
+			protection+=1;
+			elb.getEquipmentInSlot(1).damageItem(1, elb);
 		}
+		if(elb.getEquipmentInSlot(2) != null &&
+			elb.getEquipmentInSlot(2).getItem() == IaSTools.armorSpiderSilk[2]) {
+			protection+=3;
+			elb.getEquipmentInSlot(2).damageItem(3, elb);
+		}
+		if(elb.getEquipmentInSlot(3) != null &&
+			elb.getEquipmentInSlot(3).getItem() == IaSTools.armorSpiderSilk[1]) {
+			protection+=2;
+			elb.getEquipmentInSlot(3).damageItem(2, elb);
+		}
+		if(elb.getEquipmentInSlot(4) != null &&
+			elb.getEquipmentInSlot(4).getItem() == IaSTools.armorSpiderSilk[0]) {
+			protection+=2;
+			elb.getEquipmentInSlot(1).damageItem(1, elb);
+		}
+		if(e.source.isMagicDamage())
+			e.ammount = Math.max(1, e.ammount-protection);
+		if(protection > 0)
+			elb.addPotionEffect(new PotionEffect(Potion.invisibility.id, 5+protection*10));
 	}
 }
